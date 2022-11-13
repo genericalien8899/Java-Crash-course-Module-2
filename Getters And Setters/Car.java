78c1af25f37abd279e5f8b5d0ae5125a2cec6c9c
genericalien8899 <115408845+genericalien8899@users.noreply.github.com>
2022-11-14 01:44:03 +0530

    Have used the same example / scenario used for Constructor case
    The Constructors class file is edited to this DO NOT SEARCH FOR IT LOCALLY.

---
+++ Getters And Setters/Car.java
@@ -0,0 +1,47 @@
+public class Car {
+    private String make;
+    private double price;
+    private int year;
+    private String color;
+
+    public Car(String make,double price,int year, String color)
+    {
+        this.make = make;
+        this.price = price;
+        this.year = year;
+        this.color = color;
+    }
+
+    public String getMake(){
+        return this.make;
+    }
+    
+    public double getPrice(){
+        return this.price;
+    }
+
+    public int getYear(){
+        return this.year;
+    }
+
+    public String getColor(){
+        return this.color;
+    }
+
+    public void setmake(String make){
+        this.make = make;
+    }
+
+    public void setYear(int year){
+        this.year = year;
+    }
+
+    public void setPrice(double price){
+        this.price=price;
+    }
+
+    public void setColor(String color){
+        this.color = color;
+    }
+
+}
