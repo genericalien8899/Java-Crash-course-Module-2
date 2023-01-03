a2cbe9a7abbcc0ed54343defdf54d8b94deb2677
genericalien8899 <115408845+genericalien8899@users.noreply.github.com>
2023-01-03 23:49:02 +0530

    incomplete 2 error

---
+++ Section - 7 Revised/Workbook 7.8 Airline/Airline.java
@@ -0,0 +1,11 @@
+public class Airline {
+    
+    public Airline(){
+        Person[] people = new Person[11];
+    }
+
+    public Person getPerson(int index){
+        return people[index];
+    }
+
+}
